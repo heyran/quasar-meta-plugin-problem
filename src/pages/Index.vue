285dece22daf69e10ad8f2f9409356cb6a43a1e9
Zaynab <zaynab.raisi@gmail.com>
2021-04-26 11:24:16 -0700

    first commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,31 @@
+<template>
+  <q-page class="flex flex-center">
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-full.svg"
+    >
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex',
+  meta: {
+    title: 'title',
+    meta: {
+      description: {
+        name: 'description',
+        content: 'description'
+      },
+      og1: {
+        name: 'og:title',
+        content: 'og title'
+      },
+      og2: {
+        name: 'og:description',
+        content: 'og description'
+      }
+    }
+  }
+}
+</script>
